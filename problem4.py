4fe6c28f438d66652f50c32601c528f21624dd03
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-05 19:09:57 +0530

    Refactor age check and user prompts in problem4.py

--- problem4.py
+++ problem4.py
@@ -1,9 +1,5 @@
-# get age from the user
-# get name form the user
-
 name =  input("Enter your name ")
 age = int(input("Enter your age "))
-#print(type(age))
 if(age>=18):
     print("Hi "+name+" You can take part in votting")
 else:
