c1a5d9d0ba4290912cb9d88eb0ca092649ef4665
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-06 18:21:11 +0530

    Update problem11.py

--- problem11.py
+++ problem11.py
@@ -2,14 +2,10 @@
 
 print("Finding Max Number")
 n1 = int(input("Enter 1st number"))
-n2 = int(input("Enter 2nd number")) #'4' to 4
-
-#if(n1 > n2):
-#    print("First number is the greater that is "+str(n1))
-#else:
-#    print("2nd number is the greater that is "+str(n2))
+n2 = int(input("Enter 2nd number")) 
 
 print(max(n1,n2))  
+
 
 
 
