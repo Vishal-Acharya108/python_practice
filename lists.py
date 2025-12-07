ae5efe1799c06098d7e33bce05a85c5f7f319af2
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-07 16:07:18 +0530

    Fix date initialization and calculate date difference

--- lists.py
+++ lists.py
@@ -3,4 +3,10 @@ d=datetime(2016,10,23,22,57,58,342380)
 print("year=",d.year)
 print("month=",d.month)
 
+from datetime import datetime,date
+t1 date(years= 2017, month=4, day=13)
+t2 date(years= 2017, months=5, day=23)
+t3 = t2 - t1
+print(t3)
+
       
