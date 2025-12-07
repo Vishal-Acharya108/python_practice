e2f35036e3ecaf5746e8e9bdebe021f62359c8c2
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-07 17:08:38 +0530

    Remove check_references function and add set operations

--- variables 1.py
+++ variables 1.py
@@ -1,16 +1,5 @@
-def check_references():
-    # A mutable object (list)
-    list_a = [1, 2, 3]
-    list_b = list_a  # b references the same object as a
+{,1,2,3,4,5,6,7}.intersection([3,4,5,6,7})
 
-    print(f"ID of A: {id(list_a)}")
-    print(f"ID of B: {id(list_b)}")
-    list_b.append(4)
-    print(f"List A after modifying B: {list_a}")
-    list_b = [10, 20]
-    print(f"ID of B after reassignment: {id(list_b)}")
-    print(f"List A remains: {list_a}")
+{3,4,5}&{3,4,5,6}
 
-check_references()
-
-
+{3,4,5}.union{3,4,5,6}
