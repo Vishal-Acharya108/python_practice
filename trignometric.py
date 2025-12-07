b3257c6019abd35b0efb70b83d9b6f3eaf879d87
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-07 16:22:31 +0530

    Update and rename odd_even.py to trignometric.py

---
+++ trignometric.py
@@ -0,0 +1,16 @@
+a,b=1,2
+import math
+math.sin(a)
+
+math.cosh(b)
+math.atan(math.pi)
+math.hypot(a,b)
+math.degree(a)
+math.radians(57.65468)
+a,b=3,4
+a*b
+
+import operator
+operator.mul(a,b)
+
+2*'ab'
