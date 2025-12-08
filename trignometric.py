d5c1a6818060ea516c39601a1d64bd9aa297978f
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-08 19:09:24 +0530

    Reformat trignometric.py for consistency

--- trignometric.py
+++ trignometric.py
@@ -4,13 +4,19 @@ math.sin(a)
 
 math.cosh(b)
 math.atan(math.pi)
-math.hypot(a,b)
-math.degree(a)
-math.radians(57.65468)
-a,b=3,4
-a*b
 
+math.hypot(a,b)
+
+math.degree(a)
+
+math.radians(57.65468)
+
+a,b=3,4
+
+a*b
 import operator
+
 operator.mul(a,b)
 
 2*'ab'
+
