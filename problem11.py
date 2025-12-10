56e2fae4e50451dda73b53e0b66532ce9c86cce1
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2025-12-10 21:02:50 +0530

    Enhance problem11.py with example output
    Added example output for numbers 0 to 38.

--- problem11.py
+++ problem11.py
@@ -2,6 +2,11 @@ print("Finding Max Number")
 n1 = int(input("Enter 1st number"))
 n2 = int(input("Enter 2nd number"))
 print(max(n1,n2))  
+print("this is the sample where i will give examples ")
+for i in range(20):
+  print(2*i)
+  
+
 
 
 
